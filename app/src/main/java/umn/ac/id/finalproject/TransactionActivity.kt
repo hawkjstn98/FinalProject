5d643ec8b8d076ae6e3a9a1b4548f72cae249523
frapslover63 <39822385+frapslover63@users.noreply.github.com>
2019-04-21 23:50:46 +0700

    Transactiion Layout

--- app/src/main/java/umn/ac/id/finalproject/TransactionActivity.kt
+++ app/src/main/java/umn/ac/id/finalproject/TransactionActivity.kt
@@ -3,13 +3,22 @@ package umn.ac.id.finalproject
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_transaction.*
 
 class TransactionActivity : AppCompatActivity() {
 
+    lateinit var produkId: EditText
+    lateinit var ukuran: EditText
+    lateinit var jumlah: EditText
+    lateinit var warna: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_transaction)
+
+        produkId = findViewById(R.id.idItem)
+
         btnTambah.setOnClickListener{
             val intent = Intent(this@TransactionActivity, TransactionConfirmationActivity::class.java);
             startActivity(intent);
