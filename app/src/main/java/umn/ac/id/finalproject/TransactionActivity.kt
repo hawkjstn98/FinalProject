03e93b4f0b4588d9f57f28629b61660a25b6ddc7
shiteaters <shiteaters@users.noreply.github.com>
2019-04-21 23:00:06 +0700

    Camera for verification
    Verification, but recycler view still error on confirmation

--- app/src/main/java/umn/ac/id/finalproject/TransactionActivity.kt
+++ app/src/main/java/umn/ac/id/finalproject/TransactionActivity.kt
@@ -1,12 +1,19 @@
 package umn.ac.id.finalproject
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_transaction.*
 
 class TransactionActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_transaction)
+        btnTambah.setOnClickListener{
+            val intent = Intent(this@TransactionActivity, TransactionConfirmationActivity::class.java);
+            startActivity(intent);
+        }
+
     }
 }
--- app/src/main/java/umn/ac/id/finalproject/TransactionConfirmationActivity.kt
+++ app/src/main/java/umn/ac/id/finalproject/TransactionConfirmationActivity.kt
@@ -1,12 +1,36 @@
 package umn.ac.id.finalproject
 
+import android.content.Intent
+import android.graphics.Bitmap
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import kotlinx.android.synthetic.main.activity_transaction_confirmation.*
 
 class TransactionConfirmationActivity : AppCompatActivity() {
+
+    val REQUEST_IMAGE_CAPTURE=1;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_transaction_confirmation)
+        btnTambah.setOnClickListener{
+            dispatchTakePictureIntent();
+        }
+    }
+
+    private fun dispatchTakePictureIntent(){
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            val image = data?.extras?.get("data") as Bitmap;
+            viewStruk.setImageBitmap(image);
+        }
     }
 }
